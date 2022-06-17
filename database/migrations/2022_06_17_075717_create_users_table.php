e252995af061664e411bbc1c9e9a1bc25381dc0d
Adam Řezáč <rezac.adam@email.cz>
2022-06-17 10:14:00 +0200

    table animals

---
+++ database/migrations/2022_06_17_075717_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('image_id')->nullable();
+            $table->unsignedBigInteger('owner_id')->nullable();
+            $table->string('name', 100)->nullable();
+            $table->string('species', 100)->nullable();
+            $table->string('breed', 100)->nullable();
+            $table->integer('age')->nullable();
+            $table->integer('weight')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
